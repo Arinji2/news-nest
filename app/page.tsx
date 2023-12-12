28cdf4d4c4a7364621679f8c025c28ea966b2b8f
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2023-12-12 23:14:26 +0530

    Fix Mobile Heights

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default async function Home() {
   return (
-    <div className="w-full h-page bg-background flex flex-col items-center justify-center">
+    <div className="w-full h-fit md:h-page bg-background flex flex-col items-center justify-center">
       <WidthWrapper color="#" transparent>
         <div className="w-full h-fit flex flex-col md:flex-row gap-8 items-center justify-center mt-3 xl:mt-0">
           <div className="w-full h-fit flex flex-col items-center xl:items-start justify-center gap-6">
